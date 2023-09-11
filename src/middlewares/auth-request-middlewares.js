0fc76ef7be78628eab5959d511e912548c4aa8fe
ankittkamal <officiallyankit999@gmail.com>
2023-09-11 16:11:46 +0530

    added disauthenticated middleware

--- src/middlewares/auth-request-middlewares.js
+++ src/middlewares/auth-request-middlewares.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
+const { UserService } = require("../services");
 
 const { ErrorResponse } = require("../utils/common");
 const AppError = require("../utils/errors/app-error");
@@ -22,7 +23,21 @@ function validateAuthRequest(req, res, next) {
   }
   next();
 }
+async function checkAuth(req, res, next) {
+  try {
+    const response = await UserService.isAuthenticated(
+      req.headers["x-access-token"]
+    );
+    if (response) {
+      req.user = response; // setting the user id in the req object
+      next();
+    }
+  } catch (error) {
+    return res.status(error.statusCode).json(error);
+  }
+}
 
 module.exports = {
   validateAuthRequest,
+  checkAuth,
 };
