05119e39a094cdea0c341a26480a89b3e4c3e3b6
ankittkamal <officiallyankit999@gmail.com>
2023-09-11 15:54:48 +0530

    added signin with jwt creation

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,4 +4,6 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   SALT_ROUNDS: process.env.SALT_ROUNDS,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -2,4 +2,5 @@ module.exports = {
   ErrorResponse: require("./errror-response"),
   SuccessResponse: require("./success-response"),
   Enums: require("./enums"),
+  Auth: require("./auth"),
 };
