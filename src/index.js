46581500ac68f55c7967782571a3627c21ef6040
ankittkamal <officiallyankit999@gmail.com>
2023-09-11 18:52:41 +0530

    Added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { rateLimit } = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const { ServerConfig, Logger } = require("./config");
 
@@ -6,8 +8,31 @@ const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
+
+app.use(
+  "/flightService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { "^/flightService": "/" },
+  })
+);
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+  })
+);
 
 app.use("/api", apiRoutes);
 
